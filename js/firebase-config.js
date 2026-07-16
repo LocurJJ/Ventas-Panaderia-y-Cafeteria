60f75d310d7d48c64c35490986b42af99ae1b7e3
LocurJJ <127799708+LocurJJ@users.noreply.github.com>
2026-07-16 00:22:00 -0300

    Connect app to Firebase Realtime Database

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,18 +1,18 @@
 (function () {
   const config = {
-    apiKey: "",
-    authDomain: "",
-    databaseURL: "",
-    projectId: "",
-    storageBucket: "",
-    messagingSenderId: "",
-    appId: "",
+    apiKey: "AIzaSyCQR5BOgJ62Ba0s-Jq1RP2LpPOrdiADOAM",
+    authDomain: "cafeteriaypanaderia.firebaseapp.com",
+    databaseURL: "https://cafeteriaypanaderia-default-rtdb.firebaseio.com",
+    projectId: "cafeteriaypanaderia",
+    storageBucket: "cafeteriaypanaderia.firebasestorage.app",
+    messagingSenderId: "459241143036",
+    appId: "1:459241143036:web:4d8d9f3ce934edaa1e7579",
   };
 
   const isConfigured = config.databaseURL && config.projectId;
 
   if (!isConfigured) {
-    console.warn("Firebase no esta configurado todavia. PegÃ¡ la configuracion del proyecto en js/firebase-config.js.");
+    console.warn("Firebase no esta configurado todavia. Pega la configuracion del proyecto en js/firebase-config.js.");
     window.panaderiaFirebaseReady = false;
     return;
   }
